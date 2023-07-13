28684f5d7e22336896ad68fe93e9b079014ab36e
Yoba Fett <alexka.680@gmail.com>
2023-07-13 20:44:33 +0900

    Add id section, tmp for dev

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,7 +1,8 @@
 <template>
   <li :class="{ taskItem: true, complete: task.complete }">
     <div class="taskItem-info">
-      <p class="taskItem-title">{{ task.title }} {{ task.id }}</p>
+      <p class="taskItem-id">task id: {{ task.id }}</p>
+      <p class="taskItem-title">{{ task.title }}</p>
       <p class="taskItem-text">{{ task.text }}</p>
     </div>
     <div class="taskItem-controls">
@@ -11,22 +12,22 @@
 </template>
 
 <script>
-import CustomCheckbox from "./CustomCheckbox";
+import CustomCheckbox from './CustomCheckbox';
 
 export default {
-  name: "TaskItem",
-  props: ["task"],
+  name: 'TaskItem',
+  props: ['task'],
   components: { CustomCheckbox },
 };
 </script>
 
 <style scoped>
 .taskItem {
-  border: 1px solid #FCD9B8;
+  border: 1px solid #fcd9b8;
   border-radius: 1px;
   min-height: 100px;
   padding: 5px;
-  background-color: #292C35;
+  background-color: #292c35;
   box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.25);
   cursor: pointer;
   transition: all 0.25s linear;
@@ -37,33 +38,36 @@ export default {
 }
 
 .taskItem:hover {
-  border: 1px solid #E09145;
+  border: 1px solid #e09145;
   box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.5);
 }
 .taskItem:hover .taskItem-title {
-  border-bottom: 1px solid #E09145;
+  border-bottom: 1px solid #e09145;
 }
 
-.taskItem:hover .taskItem-title,
-.taskItem:hover .taskItem-text {
-  color: #E09145;
+.taskItem:hover .taskItem-info > * {
+  color: #e09145;
+  transition: all 0.25s linear;
 }
 
 .taskItem-info {
 }
 
+.taskItem-id {
+  font-size: 8px;
+}
+
 .taskItem-title {
   font-size: 22px;
   font-weight: 700;
-  border-bottom: 1px solid #FCD9B8;
+  border-bottom: 1px solid #fcd9b8;
   margin-bottom: 5px;
-  transition: all 0.25s linear;
+  
 }
 
 .taskItem-text {
   padding-right: 30px;
   margin-bottom: 30px;
-  transition: all 0.25s linear;
 }
 
 .taskItem-controls {
