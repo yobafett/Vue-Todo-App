e7e838ec2584f56ee9f7ebb606fccbf49b08c64c
Yoba Fett <alexka.680@gmail.com>
2023-07-15 15:51:36 +0900

    Rework

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,11 +1,16 @@
 <template>
   <li :class="{ taskItem: true, complete: task.complete }">
-    <div class="taskItem-info">
-      <p class="taskItem-id">task id: {{ task.id }}</p>
-      <p class="taskItem-title">{{ task.title }}</p>
-      <p class="taskItem-text">{{ task.text }}</p>
+    <div class="header">
+      <div>
+        <p class="id">task id: {{ task.id }}</p>
+        <p class="title">{{ task.title }}</p>
+      </div>
+      <p class="delBtn">🗙</p>
     </div>
-    <div class="taskItem-controls">
+
+    <p class="text">{{ task.text }}</p>
+
+    <div class="controls">
       <CustomCheckbox :checked="task.complete" />
     </div>
   </li>
@@ -21,58 +26,60 @@ export default {
 };
 </script>
 
-<style scoped>
-.taskItem {
-  border: 1px solid #fcd9b8;
-  border-radius: 1px;
-  min-height: 100px;
-  padding: 5px;
-  background-color: #292c35;
-  box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.25);
-  cursor: pointer;
-  transition: all 0.25s linear;
-}
-
+<style scoped lang="scss">
 .complete {
   opacity: 0.25;
 }
-
-.taskItem:hover {
-  border: 1px solid #e09145;
-  box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.5);
-}
-.taskItem:hover .taskItem-title {
-  border-bottom: 1px solid #e09145;
-}
-
-.taskItem:hover .taskItem-info > * {
-  color: #e09145;
+.taskItem {
+  border: 1px solid #fcd9b8;
+  padding: 10px;
+  border-radius: 1px;
+  min-height: 100px;
+  background-color: #292c35;
+  box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.25);
   transition: all 0.25s linear;
-}
-
-.taskItem-info {
-}
-
-.taskItem-id {
-  font-size: 8px;
-}
-
-.taskItem-title {
-  font-size: 22px;
-  font-weight: 700;
-  border-bottom: 1px solid #fcd9b8;
-  margin-bottom: 5px;
-  
-}
-
-.taskItem-text {
-  padding-right: 30px;
-  margin-bottom: 30px;
-}
-
-.taskItem-controls {
-  padding: 5px;
-  display: flex;
-  justify-content: end;
+  & * {
+    transition: all 0.25s linear;
+  }
+  &:hover {
+    box-shadow: 3px 3px 1px rgba(224, 145, 69, 0.75);
+  }
+  .header {
+    border-bottom: 1px solid #fcd9b8;
+    padding-bottom: 5px;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    .id {
+      font-size: 8px;
+    }
+    .title {
+      font-size: 22px;
+      font-weight: 700;
+      cursor: pointer;
+      &:hover {
+        color: #e09145;
+      }
+    }
+    .delBtn {
+      font-size: 30px;
+      line-height: 30px;
+      cursor: pointer;
+      &:hover {
+        color: #e09145;
+      }
+    }
+  }
+  .text {
+    margin-bottom: 30px;
+    cursor: pointer;
+    &:hover {
+      color: #e09145;
+    }
+  }
+  .controls {
+    display: flex;
+    justify-content: end;
+  }
 }
 </style>
