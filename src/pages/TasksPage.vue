5271ac6c0a4bd157c537d22f744bebef50437e54
Yoba Fett <alexka.680@gmail.com>
2023-07-23 16:14:38 +0900

    Task page component

---
+++ src/pages/TasksPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <TaskList />
+    <AddForm @create="addTask" />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import AddForm from '@/components/AddForm.vue';
+import TaskList from '@/components/TaskList.vue';
+
+export default {
+  name: 'App',
+  components: { AddForm, TaskList },
+  methods: {
+    ...mapActions({
+      addTask: 'addTask',
+    }),
+  },
+};
+</script>
+
+<style scoped>
+</style>
