b5011dee473a6c54b3a0f0def1bdd68c90a00733
Yoba Fett <alexka.680@gmail.com>
2023-07-23 16:14:16 +0900

    PageHeader component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="pageHeader">
+    <div class="nav">
+      <h1 @click="$router.push('/')">Todo app</h1>
+      <h2 @click="$router.push('/tasks')">Task list</h2>
+    </div>
+    <ColorScheme :colors="colors" />
+  </div>
+</template>
+
+<script>
+import ColorScheme from '@/components/ColorScheme.vue';
+
+export default {
+  name: 'PageHeader',
+  components: { ColorScheme },
+  data() {
+    return {
+      colors: ['#fcd9b8', '#e09145', '#292C35', '#17181d'],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.pageHeader {
+  margin: 0px 20px;
+  margin-bottom: 15px;
+  padding-bottom: 5px;
+  border-bottom: 1px solid #fcd9b8;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  .nav {
+    h1 {
+      font-size: 72px;
+      margin-right: 25px;
+      padding-right: 25px;
+    }
+    h2 {
+      font-size: 32px;
+    }
+    * {
+      cursor: pointer;
+      transition: all 0.25s linear;
+      &:hover {
+        color: #e09145;
+      }
+    }
+  }
+}
+</style>
